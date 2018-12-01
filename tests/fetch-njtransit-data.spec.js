fb058fec4bd971c3a8819e15eaeae5710da03a41
Dmitry Studynskyi <dms@smartling.com>
2018-12-01 17:08:45 -0500

    WIP. Added incompleted tests.

--- tests/fetch-njtransit-data.spec.js
+++ tests/fetch-njtransit-data.spec.js
@@ -1,13 +1,30 @@
+/* A MagicMirror module to show bus, luas and rail arrival times.
+ * Copyright (C) 2018 Dmitry Studynskyi
+ * License: GNU General Public License */
+
 const { expect } = require("chai");
+const sinon = require("sinon");
 const NjtFetcher = require("../classes/fetch-njtransit-data");
+const njtParser = require("../classes/parse-njtransit-data");
 
 describe("Test fetcher", () => {
+    before(() => {
+        this.clock = sinon.useFakeTimers();
+    });
+
+    after(() => {
+        this.clock.restore();
+    });
+
     beforeEach(() => {
         this.fetcher = new NjtFetcher(123, 10, 158, "dir", "dest", 10, 60);
     });
 
-    it("Non xml file", () => {
-       this.fetcher.fetchStop();
+    it("Non xml file", async () => {
+        // sinon.stub(njtParser, "parseNjtData")
+        //     .callsFake(() => "mock");
+        njtParser = sinon.stub();
+        await this.fetcher.fetchStop();
     });
     it("Non xml file", () => {
         // this.fetcher.exclude(null);
